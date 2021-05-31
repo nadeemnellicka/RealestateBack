93654f7265126c9bfd5078b691ece64e699ae769
nadeemnellicka <nadeemnellicka@gmail.com>
2021-05-31 05:39:41 +0000

    changes

--- public/deploy.php
+++ public/deploy.php
@@ -44,7 +44,7 @@ foreach ($allowed_ips as $allow) {
         break;
     }
 }
-
+ 
 if (!$allowed && False) {
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
